ec117433463afea00e34e1ad7b8389bb15df4805
Charlie Teh <charlesqyt@gmail.com>
2018-10-31 10:37:53 +0800

    DateUtil py file added

---
+++ dateutil.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 10:24:29 2018
+
+@author: Tester
+"""
+
+import datetime
+
+currentDate = datetime.date.today()
+#strftime allows you to specify the date format
+print (currentDate.strftime("Please attend our event %A, %B %d in the year %Y"))
