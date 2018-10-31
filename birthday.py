69a4e34c60878fe17db7fc1a15cba02f2a480aab
Charlie Teh <charlesqyt@gmail.com>
2018-10-31 10:59:07 +0800

    birthday days countdown updated

--- birthday.py
+++ birthday.py
@@ -14,3 +14,8 @@ birthdate = datetime.datetime.strptime(birthday, "%d/%m/%Y").date()
 #which is part of the datetime class
 #which is in the datetime module
 print ("Your birth month is " + birthdate.strftime('%B'))
+
+#nextBirthday = datetime.datetime.strptime('20/12/2018', '%d/%m/%Y').date()
+currentDate = datetime.date.today()
+#If you subtract two dates you get back the number of days between those dates
+print (birthdate - currentDate)
