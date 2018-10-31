737bfc11773eb6662910f77f229de80413c282da
Charlie Teh <charlesqyt@gmail.com>
2018-10-31 11:06:00 +0800

    TimeDelta added

--- dateutil.py
+++ dateutil.py
@@ -10,3 +10,9 @@ import datetime
 currentDate = datetime.date.today()
 #strftime allows you to specify the date format
 print (currentDate.strftime("Please attend our event %A, %B %d in the year %Y"))
+
+currentDate = datetime.date.today()
+#timedelta allows you to specify the time to add or subtract  from a date
+
+print ("Timedelta 15 days : ")
+print(currentDate + datetime.timedelta (days=15))
