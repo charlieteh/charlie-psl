af8a264ed87d76a321ecf4377a6275160d3e93de
Charlie Teh <charlesqyt@gmail.com>
2018-10-31 11:22:31 +0800

    ifstatement py file added

---
+++ ifstatement.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 11:18:09 2018
+
+@author: Tester
+"""
+
+answer = input ("Would you like express shipping?")
+if answer == "yes" :
+    print ("That will be an extra $10")
+    
