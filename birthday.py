aebdd35086fed342f6bca83d1c3d4cb23ba888b4
Charlie Teh <charlesqyt@gmail.com>
2018-10-31 10:51:44 +0800

    birthday py file updated

--- birthday.py
+++ birthday.py
@@ -7,7 +7,7 @@ Created on Wed Oct 31 10:41:32 2018
 
 import datetime
 
-birthday = input ("What is your birthday (d/m/Y)? ")
+birthday = input ("When is your birthday (d/m/Y)? ")
 birthdate = datetime.datetime.strptime(birthday, "%d/%m/%Y").date()
 #why did we list datetime twice?
 #because we are calling the strptime function
