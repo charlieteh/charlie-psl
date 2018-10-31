2f9dcca2698ee2ebb8190128768ee71c59bc9af0
Charlie Teh <charlesqyt@gmail.com>
2018-10-31 11:10:17 +0800

    timeutil.py file added

---
+++ timeutil.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 11:08:02 2018
+
+@author: Tester
+"""
+
+import datetime
+
+currentTime = datetime.datetime.now()
+
+print (currentTime)
+print (currentTime.hour)
+print (currentTime.minute)
+print (currentTime.second)
+       
