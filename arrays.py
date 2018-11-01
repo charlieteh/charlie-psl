65f240bf11ac7273f25cbe47e782d2aaec402f90
Charlie Teh <charlesqyt@gmail.com>
2018-11-01 11:44:52 +0800

    arrays.py file added

---
+++ arrays.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 11:35:19 2018
+
+@author: Tester
+"""
+
+guests = ['first','second','third']
+
+#first item in the list starts with 0
+print(guests[0])
+print(guests[1])
+print(guests[2])
+
+#printing last item in the list
+print(guests[-1])
+
+#printing 2nd last item in the list
+print(guests[-2])
