db70ff976cf13d574fdc93337d66662e056f5a4d
Charlie Teh <charlesqyt@gmail.com>
2018-11-01 10:35:59 +0800

    turtle.py file added

---
+++ turtle.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 09:55:36 2018
+
+@author: Tester
+"""
+
+import turtle
+
+for steps in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+
+
+turtle.color('red')
+turtle.forward(200)
+
