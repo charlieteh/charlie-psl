6069842b0730e19c4b95d2cc31348d54626e91e5
Charlie Teh <charlesqyt@gmail.com>
2018-11-01 11:21:47 +0800

    whileloop.py fileadded

---
+++ whileloop.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 11:05:35 2018
+
+@author: Tester
+"""
+
+answer = 0
+
+while answer != 4:
+    answer= int(input("What is 2+2?\n"))
+    
+print ("Yes! 2+2 = 4")
+    
