c6002250a2c604f164a93af879db2943301dace5
charlieteh <charlesqyt@gmail.com>
2018-11-05 11:27:42 +0800

    function.py file updated

--- function.py
+++ function.py
@@ -7,8 +7,24 @@ Created on Mon Nov  5 10:58:19 2018
 
 #Create a function to print hello
 
-def printMessage():
-    print("Hello World")
+def printMessage(name):
+    print(name + ", " + "Hello there.")
     return
 
-printMessage()
+printMessage("Charlie")
+
+def addingNumbers (var1, var2):
+    total = var1 + var2
+    return total
+
+print(addingNumbers(3,4))
+
+def printinfo(name, age):
+    "This prints a passed info into thois function"
+    print("Name: ", name)
+    print("Age: ", age)
+    return ;
+
+printinfo("Charlie", 30)
+
+printinfo(age=28, name="Carlos")
