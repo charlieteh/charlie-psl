6b7f05dd9b1b2e8e48f68f7b705a4885e72ce125
charlieteh <charlesqyt@gmail.com>
2018-11-05 11:05:27 +0800

    function.py file added

---
+++ function.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  5 10:58:19 2018
+
+@author: Charles.Teh
+"""
+
+#Create a function to print hello
+
+def printMessage():
+    print("Hello World")
+    return
+
+printMessage()
