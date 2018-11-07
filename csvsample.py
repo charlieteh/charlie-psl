10031a4e27184135ad5bd91fd3c74c7194f18e64
charlieteh <charlesqyt@gmail.com>
2018-11-07 10:28:40 +0800

    csvsample.py file added

---
+++ csvsample.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 10:00:59 2018
+
+@author: Charles.Teh
+"""
+
+import csv
+
+myCSVfile = open('data/demo.csv','r')
+dataFromFile = csv.reader(myCSVfile)
+
+print(dataFromFile)
+
+
+accessMode = 'r'
+"""
+with open(fileName, accessMode) as myCSVfile:
+    #Read the file contents
+    dataFromFile = csv.reader(myCSVfile)
+    #For loop that will run once per row
+    for row in dataFromFile:
+        print(row)
+
+"""
+
+for row in dataFromFile:
+    print(row)
+    
+myCSVfile.close()
