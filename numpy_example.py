ec89cd11d6c1bf1c776a1c3d838b3cd29cc32ef2
charlieteh <charlesqyt@gmail.com>
2018-11-14 11:47:35 +0800

    numpy_example.py file added

---
+++ numpy_example.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 14 11:37:32 2018
+
+@author: Charles.Teh
+"""
+
+import numpy as np
+
+x = np.float32(1.0)
+print(x)
+
+y = np.int_([1,2,4])
+print(y)
+
+z = np.arange(3,dtype=np.uint8)
+print(z)
+
+c = np.arange(9).reshape(3,3)
+print(c)
